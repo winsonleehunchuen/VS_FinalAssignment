7b6b824061b2f85d6e7f18921f8101209fe94c73
HOO KAH HIEN <hookahhien1119@e.djzhlc.edu.my>
2020-08-17 01:11:28 +0800

    database is open

---
+++ VS_FinalAssignment/VS_FinalAssignment/DatabaseConnection.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace VS_FinalAssignment
+{
+    public class DatabaseConnection
+    {
+        private string connectionString;
+
+        public DatabaseConnection()
+        {
+            connectionString = "Data Source=JIAXIAN;Initial Catalog=FinalAssignment;User ID=sa;Password=123";
+            //connectionString = @"Data Source=LAPTOP-OM5J60IG\SQLSERVER;Initial Catalog=FinalAssignment;Integrated Security = true";
+        }
+
+        public DatabaseConnection(string connectionString)
+        {
+            this.connectionString = connectionString;
+        }
+
+        //Get Data
+        public DataTable getDataSQL(string query)
+        {
+            DataTable dt = new DataTable();
+            SqlConnection connection = new SqlConnection(connectionString);
+            connection.Open();
+            SqlCommand command = new SqlCommand(query, connection);
+            dt.Load(command.ExecuteReader());
+            connection.Close();
+            return dt;
+        }
+
+        //insert, update, delete
+        public void executeSQL(string query)
+        {
+            SqlConnection connection = new SqlConnection(connectionString);
+            connection.Open();
+            SqlCommand command = new SqlCommand(query, connection);
+            command.ExecuteNonQuery();
+            connection.Close();
+        }
+    }
+}
