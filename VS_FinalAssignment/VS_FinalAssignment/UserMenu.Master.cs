6f8764470f56f33a06463145fa4f506d41e04bee
Winson Lee <hunchuenlee@gmail.com>
2020-08-16 13:32:45 +0800

    Template Design and UserMenu.Master Coding

---
+++ VS_FinalAssignment/VS_FinalAssignment/UserMenu.Master.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace VS_FinalAssignment
+{
+    public partial class UserMenu : System.Web.UI.MasterPage
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (Session["userN"] != null)
+            {
+                username.InnerText = (string)Session["userN"];
+            }
+            /*if (Session["userN"] == null)
+            {
+                Response.Redirect("Login.aspx");
+            }*/
+        }
+
+        protected void LinkButton1_Click(object sender, EventArgs e)
+        {
+            Session.Clear();
+            Response.Redirect("UserHome.aspx");
+        }
+    }
+}
